7c68016f26dce262a3447f6ea0f7d438d78199e8
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-09-06 21:50:00 +0800

    Optimisations to urls obtaining code so that it does not perform too many unnecessary operations

--- scoutappimprovedv2/lib/logic/scout_badge_manager.dart
+++ scoutappimprovedv2/lib/logic/scout_badge_manager.dart
@@ -7,7 +7,6 @@ import 'package:googleapis_auth/auth_io.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:scoutappimprovedv2/logic/scout_badge/scout_badge.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../sensitive.dart';
 
@@ -65,7 +64,6 @@ class ScoutBadgeManager {
 
   Stream<ScoutBadge?> parse(GoogleSignInAccount? account) async* {
     var db = await _getDB();
-    SharedPreferences prefs = await SharedPreferences.getInstance();
 
     // if (!(prefs.getBool("already_processing_badges") ?? false)) {
     // prefs.setBool("already_processing_badges", true);
@@ -79,106 +77,109 @@ class ScoutBadgeManager {
 
     await Future.delayed(const Duration(seconds: 5));
 
-    var urls =
-        await headlessWebView.webViewController.evaluateJavascript(source: """
+    if (_parsedUrls.isEmpty) {
+      var urls =
+          await headlessWebView.webViewController.evaluateJavascript(source: """
               JSON.stringify(
                 Array.from(document.getElementsByTagName('a'))
                   .map(img => img.href)
               )
               """);
-    _parsedUrls = List<String>.from(jsonDecode(urls));
+      _parsedUrls = List<String>.from(jsonDecode(urls));
 
-    // Filter out the garbage
-    _parsedUrls.removeWhere((element) =>
-        element == "https://scout.sg/" ||
-        element == "http://intranet.scout.org.sg/");
+      // Filter out the garbage
+      _parsedUrls.removeWhere((element) =>
+          element == "https://scout.sg/" ||
+          element == "http://intranet.scout.org.sg/");
 
-    _parsedUrls.removeWhere((element) {
-      List<ScoutBadge> list =
-          (db.scoutBadges.filter().urlEqualTo(element).findAllSync());
+      _parsedUrls.removeWhere((element) {
+        List<ScoutBadge> list =
+            (db.scoutBadges.filter().urlEqualTo(element).findAllSync());
 
-      if (list.isEmpty) {
-        // Means it is not inside
-        return false;
-      } else {
-        return true;
-      }
-    });
+        if (list.isEmpty) {
+          // Means it is not inside
+          return false;
+        } else {
+          return true;
+        }
+      });
+      await headlessWebView.dispose();
+    }
 
-    headlessWebView.dispose();
-    for (String i in _parsedUrls) {
-      HeadlessInAppWebView tempView = HeadlessInAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse(i)),
-      );
+    var urls = _parsedUrls.map((e) => e).toList();
 
-      await tempView.run();
-
-      await Future.delayed(const Duration(milliseconds: 5000));
-
-      var image =
-          await tempView.webViewController.evaluateJavascript(source: """
-                JSON.stringify(
-                Array.from(document.getElementsByTagName('img'))
-                  .map(img => img.src)
-              )
-              """);
-
-      var name = await tempView.webViewController.evaluateJavascript(source: """
-                JSON.stringify(
-                Array.from(document.getElementsByTagName('h2'))
-                  .map(h2 => h2.textContent)
-              )
-                """);
-
-      var desc = await tempView.webViewController.evaluateJavascript(source: """
-      const spanElement = document.querySelector('span.ng-binding'); 
-
-const elementsInsideSpan = Array.from(spanElement.childNodes);
-
-const contentArray = elementsInsideSpan.map(node => {
-  if (node.nodeType === Node.TEXT_NODE) {
-    return node.textContent.trim();
-  } else if (node.nodeType === Node.ELEMENT_NODE) {
-    return node.outerHTML;
-  }
-});
-
-JSON.stringify(contentArray);
-
-      """);
-
-      String imageURL = List<String>.from(jsonDecode(image))[0];
-      String badgeName = List<String>.from(jsonDecode(name))[0];
-      String description = List<String>.from(jsonDecode(desc)).join("");
-
-      var newBadge = ScoutBadge()
-        ..name = badgeName
-        ..url = i
-        ..description = description
-        ..imageURL = imageURL
-        ..parsedGoogleSheetInfo = false
-        ..completed = null
-        ..badgeGiven = null
-        ..certGiven = null;
-
-      // Add new badge to the database
-
-      // await db.scoutBadges.put(newBadge);
-
-      yield newBadge;
-
-      print(newBadge.name);
-
-      tempView.dispose();
+    for (String i in urls) {
+      yield (await parseScoutDataUrl(i));
+      _parsedUrls.remove(i);
     }
 
     if (account != null) {
       await updateFromGSheets(account);
     }
 
-    // Parser Successfully Completed
-    // prefs.setBool("already_processing_badges", false);
-    // }
+    _parsedUrls = [];
+  }
+
+  Future<ScoutBadge> parseScoutDataUrl(String i) async {
+    HeadlessInAppWebView tempView = HeadlessInAppWebView(
+      initialUrlRequest: URLRequest(url: Uri.parse(i)),
+    );
+
+    await tempView.run();
+
+    await Future.delayed(const Duration(milliseconds: 5000));
+
+    var image = await tempView.webViewController.evaluateJavascript(source: """
+                JSON.stringify(
+                Array.from(document.getElementsByTagName('img'))
+                  .map(img => img.src)
+              )
+              """);
+
+    var name = await tempView.webViewController.evaluateJavascript(source: """
+              JSON.stringify(
+              Array.from(document.getElementsByTagName('h2'))
+                .map(h2 => h2.textContent)
+            )
+              """);
+
+    var desc = await tempView.webViewController.evaluateJavascript(source: """
+    const spanElement = document.querySelector('span.ng-binding'); 
+    
+    const elementsInsideSpan = Array.from(spanElement.childNodes);
+    
+    const contentArray = elementsInsideSpan.map(node => {
+      if (node.nodeType === Node.TEXT_NODE) {
+        return node.textContent.trim();
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        return node.outerHTML;
+      }
+    });
+    
+    JSON.stringify(contentArray);
+    
+    """);
+
+    String imageURL = List<String>.from(jsonDecode(image))[0];
+    String badgeName = List<String>.from(jsonDecode(name))[0];
+    String description = List<String>.from(jsonDecode(desc)).join("");
+
+    var newBadge = ScoutBadge()
+      ..name = badgeName
+      ..url = i
+      ..description = description
+      ..imageURL = imageURL
+      ..parsedGoogleSheetInfo = false
+      ..completed = null
+      ..badgeGiven = null
+      ..certGiven = null;
+
+    // Add new badge to the database
+
+    // await db.scoutBadges.put(newBadge);
+
+    tempView.dispose();
+    return newBadge;
   }
 
   Future<void> updateFromGSheets(GoogleSignInAccount account) async {
